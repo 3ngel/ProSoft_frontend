4e971fb8a8022a60c2914ef6e803969b6c5f7f94
3ngel <angelina_panfilova@bk.ru>
2025-12-06 11:58:07 +0500

    pup

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,7 @@
 // import { useRoute, useRouter } from 'vue-router';
 
-
-// const router = useRouter()
+const md5 = require("md5");
 const logins = {login:'user', password:'1'}
-// const key = '52495A2D33333039313675'
-// const CryptoJS = require('crypto-js'); 
 Vue.createApp({
     data() {
         const config = require("./config.json")
@@ -24,30 +21,26 @@ Vue.createApp({
         },
         async authorization () {
         if(this.login === '' || this.password ==='') { alert("Вы не ввели логин или пароль"); return};
-        // const hash = CryptoJS.HmacSHA256(this.password, key).toString();
-        // console.log("Ваш логин "+this.login+"\nВаш пароль "+hash);
-        console.log("Ваш логин "+this.login+"\nВаш пароль "+this.password);
-        // запрос к back
-        const response = await axios.post(this.api_url+'/user_verify', {
-          login: this.username,
-          password: this.password
-        });
-        //Успешная аутентификация
-        if(response.error==null){
-        // if(this.login == logins.login && this.password == logins.password){
-            if (navigator.cookieEnabled === false){
-	            alert("Cookies отключены!");
+            console.log("Ваш логин "+this.login+"\nВаш пароль "+this.password);
+            // запрос к back
+            const response = await axios.post(this.api_url+'/user_verify', {
+            login: this.username,
+            password: md5(this.password) //Хеш от пароля
+            });
+            //Успешная аутентификация
+            if(response.error==null){
+                if (navigator.cookieEnabled === false){
+                    alert("Cookies отключены!");
+                }
+                //Задаю куки авторизованного пользователя
+                document.cookie = "user_id="+response.user_id
+                document.cookie = "user="+response.user
+                this.login = this.password = '';            
+                window.location = './pages/activites_list/activites_list.html'
             }
-            //Задаю куки авторизованного пользователя
-            document.cookie = "user_id="+response.user_id
-            document.cookie = "user="+response.user
-            this.login = this.password = '';            
-            window.location = './pages/activites_list/activites_list.html'
-        }
-        else{
-            alert("Логин и/или пароль неверны")
-        }
-        // router.push({path:'./pages/user_browse/user_browse.html'})
+            else{
+                alert("Логин и/или пароль неверны")
+            }
         },
     }
 }
